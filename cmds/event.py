8cdc2042f2e07fc1277a9ee9cde32d03af7982ff
we77465 <68180138+we77465@users.noreply.github.com>
2023-08-10 21:37:35 +0800

    偵測

--- cmds/event.py
+++ cmds/event.py
@@ -44,7 +44,7 @@ class Events(Cog_extension):
                 },
                 json = {
                     'model': 'text-davinci-003',
-                    'prompt': f'{msg.content}這段文字中 哪段是url 並回傳並且不要打其他任何字',
+                    'prompt': f'{msg.content}這段文字中 哪段是url 並回傳並且不要打其他任何字 也不要有任何換行',
                     'temperature': 0.4,
                     'max_tokens': 300
                 }
@@ -52,7 +52,38 @@ class Events(Cog_extension):
             json = response.json()
             #await msg.channel.send(response.text)
             #await msg.channel.send(response.status_code)
-            await msg.channel.send(json['choices'][0]['text'])
+            #await msg.channel.send(json['choices'][0]['text'])
+
+            API_KEY = jdata["google_api_key"]
+
+            url = json['choices'][0]['text']
+            safe_browsing_url = f"https://safebrowsing.googleapis.com/v4/threatMatches:find?key={API_KEY}"
+            payload = {
+                "client": {
+                    "clientId": "your-client-id",
+                    "clientVersion": "1.5.2"
+                },
+                "threatInfo": {
+                    "threatTypes": ["MALWARE", "SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE", "POTENTIALLY_HARMFUL_APPLICATION"],
+                    "platformTypes": ["ANY_PLATFORM"],
+                    "threatEntryTypes": ["URL"],
+                    "threatEntries": [{"url": url}]
+                }
+            }
+            response = requests.post(safe_browsing_url, json=payload)
+            response_data = response.json()
+
+            url_to_check = url
+
+            if "matches" in response_data:
+                await msg.channel.send(f"The URL '{url_to_check}' is considered unsafe.")
+                await msg.channel.send("Threats:")
+                for match in response_data["matches"]:
+                    await msg.channel.send(f"- Threat type: {match['threatType']}")
+                    await msg.channel.send(f"- Platform type: {match['platformType']}")
+                    await msg.channel.send(f"- Threat entry type: {match['threatEntryType']}")
+            else:
+                await msg.channel.send(f"{url_to_check} is safe.")            
 
             
 
