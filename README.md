5e67bb09d882869497e36687b4ccf7843e7e7b99
we77465 <68180138+we77465@users.noreply.github.com>
2023-08-12 22:14:48 +0800

    在每個小時的5.10.15...分鐘送出訊息

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # bang_bot
-我想做做看bot
-這是一個關於我想製作bot的故事 原因是AIS3讓我的衝擊太大ㄌ
+這是一個關於我想製作bot的故事 原因是AIS3讓我的衝擊太大ㄌ 再加上看能不能混過專題:D
+模組的話就pip install discord.py 差不多就J樣ㄌ
--- cmds/looop.py
+++ cmds/looop.py
@@ -20,22 +20,19 @@ class MyCog(Cog_extension):
         self.printer.cancel()
 
     @commands.command()
-    async def set_channel(self,ctx,msg:int):
+    async def set_channel(self, ctx, msg: int):
         self.channel = self.bot.get_channel(msg)
         await ctx.send(f"change to {self.channel.mention}")
 
-
-    @tasks.loop(seconds=5.0)
+    @tasks.loop(seconds=1.0)  # Run every second for accuracy
     async def printer(self):
-        print(self.index)
-        if(self.index==0):
-            await self.send_message()
-        print(self.channel)
-        await self.channel.send(self.channel)
-        await self.channel.send(self.index)
-        #await self.channel.send(self.index)
-        self.index += 1
-
+        now = datetime.datetime.now()
+        if now.minute % 5 == 0 and now.second == 0:  # At exact 5th, 10th, 15th, ... minute
+            print(self.index)
+            if self.index == 0:
+                await self.send_message()
+            await self.channel.send(self.index)
+            self.index += 1
 
     async def send_message(self):
         self.channel = self.bot.get_channel(int(jdata["WELCOME"]))
@@ -47,6 +44,54 @@ class MyCog(Cog_extension):
 
 async def setup(bot):
     await bot.add_cog(MyCog(bot))
+
+#import discord
+#from discord.ext import commands, tasks
+#from core.classes import Cog_extension
+#import pymysql
+#import asyncio
+#import datetime
+#import json
+#
+#with open('setting.json', 'r', encoding='utf-8') as jfile:
+#    jdata = json.load(jfile)
+#
+#class MyCog(Cog_extension):
+#    def __init__(self, *args, **kwargs):
+#        super().__init__(*args, **kwargs)
+#        self.channel = 0
+#        self.index = 0
+#        self.printer.start()
+#
+#    def cog_unload(self):
+#        self.printer.cancel()
+#
+#    @commands.command()
+#    async def set_channel(self,ctx,msg:int):
+#        self.channel = self.bot.get_channel(msg)
+#        await ctx.send(f"change to {self.channel.mention}")
+#
+#
+#    @tasks.loop(seconds=5.0)
+#    async def printer(self):
+#        print(self.index)
+#        if(self.index==0):
+#            await self.send_message()
+#        await self.channel.send(self.index)
+#        #await self.channel.send(self.index)
+#        self.index += 1
+#
+#
+#    async def send_message(self):
+#        self.channel = self.bot.get_channel(int(jdata["WELCOME"]))
+#
+#    @printer.before_loop
+#    async def before_printer(self):
+#        print('waiting...')
+#        await self.bot.wait_until_ready()
+#
+#async def setup(bot):
+#    await bot.add_cog(MyCog(bot))
 
 
 
