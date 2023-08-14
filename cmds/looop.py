bd51e7974c34f0982059aba46a65654490ac06e0
we77465 <68180138+we77465@users.noreply.github.com>
2023-08-14 19:25:53 +0800

    日期正常出現

--- cmds/looop.py
+++ cmds/looop.py
@@ -16,9 +16,26 @@ class MyCog(Cog_extension):
         self.channel = 0
         self.index = 0
         self.printer.start()
+        self.db_config = {
+            "host": jdata["host"],
+            "user": jdata["user"],
+            "password": "",
+            "database": jdata["database"]
+        }
+        self.connection = pymysql.connect(**self.db_config)
+        self.currentuser = "worstking99"
+        self.min = -1
 
-    def cog_unload(self):          #取消loop
+
+    @commands.command()
+    async def unloop(self,ctx):          #取消loop
         self.printer.cancel()
+        await ctx.channel.send("cancel success")
+
+    @commands.command()
+    async def loop(self,ctx):          #loop
+        self.printer.start()
+        await ctx.channel.send("loop success")
 
     @commands.command()            #設定channel要輸出的頻道
     async def set_channel(self, ctx, msg: int):
@@ -30,17 +47,45 @@ class MyCog(Cog_extension):
     @tasks.loop(seconds=1.0)  # 每一秒執行 如果在分%5==0的時候 send東西 
     async def printer(self):
         now = datetime.datetime.now()
-        if now.minute % 5 == 0 and now.second == 0:  # At exact 5th, 10th, 15th, ... minute
+        if now.minute % 5 == 6 or now.second == 0 or now.second == 30:  # At exact 5th, 10th, 15th, ... minute
             
             print(self.index)
             if self.index == 0:
                 await self.send_message()
-            await self.channel.send(self.index)
+            await self.channel.send(f"現在時間{now.hour}點{now.minute}分")
             self.index += 1
+
+            with self.connection.cursor() as cursor:
+                # 執行查詢，擷取名為 "worstking99" 的記錄
+                sql = "SELECT * FROM my_note2 WHERE name = %s ORDER BY year ASC, month ASC, day ASC,hours ASC,mins ASC"
+                cursor.execute(sql, (f'{self.currentuser}',))
+
+                # 取得所有符合條件的記錄
+                records = cursor.fetchall()
+
+                # 輸出擷取的記錄
+                for record in records:
+                    #if(record['month'])
+                    await self.channel.send(record)
+                    await self.channel.send(f"name: {str(record[1])}")
+                    await self.channel.send(f"year:{str( record[2])}")
+                    await self.channel.send(f"month:{str( record[3])}")
+                    await self.channel.send(f"day:{str( record[4])}")
+                    #print("Name:", record['name'])
+                    #
+                    #print("Month:", type(record['month']))
+                    #print("Day:", record['day'])
+                
+            
 
     async def send_message(self):
         self.channel = self.bot.get_channel(int(jdata["WELCOME"]))
 
+    
+    @commands.command()
+    async def change_user(self,ctx):
+        self.currentuser(ctx.author.name)
+        await ctx.channel.send("change success")
     
 
 
@@ -48,19 +93,7 @@ class MyCog(Cog_extension):
     async def before_printer(self):
         print('waiting...')
         await self.bot.wait_until_ready()
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
 
@@ -85,16 +118,10 @@ class MyCog(Cog_extension):
     async def show_note(self,ctx):
         await ctx.channel.send("link success")
 
-        # 修改为你的数据库连接信息
-        db_config = {
-            "host": "localhost",
-            "user": jdata["user"],
-            "password": "",
-            "database": jdata["database"]
-        }
+        
 
         try:
-            connection = pymysql.connect(**db_config)
+            connection = pymysql.connect(**self.db_config)
             print("Successfully connected to the database.")
 
             cursor = connection.cursor()
@@ -115,18 +142,18 @@ class MyCog(Cog_extension):
     @commands.command()
     async def add_note(self,ctx,year:int,month:int,day:int):
         name = ctx.author.name
-        db_config = {
-            "host": "localhost",
-            "user": jdata["user"],
-            "password": "",
-            "database": jdata["database"]
-        }
+        #db_config = {
+        #    "host": jdata["host"],
+        #    "user": jdata["user"],
+        #    "password": "",
+        #    "database": jdata["database"]
+        #}
         #await ctx.channel.send(self.is_valid_date(year, month, day))
         if self.is_valid_date(year, month, day):
             try:
                 insert_query = f"INSERT INTO my_note2 (name, year ,month, day) VALUES (%s, %s, %s, %s)"
                 values = (name,int(year) ,int(month), int(day))
-                connection = pymysql.connect(**db_config)
+                connection = pymysql.connect(**self.db_config)
                 print("Successfully connected to the database.")
 
                 cursor = connection.cursor()
@@ -142,22 +169,51 @@ class MyCog(Cog_extension):
                 connection.rollback()
         else:
             await ctx.channel.send("無效的日期")
-
+#@commands.command()
+#    async def add_note(self,ctx,year:int,month:int,day:int,hours:int,mins:int):
+#        name = ctx.author.name
+#        #db_config = {
+#        #    "host": jdata["host"],
+#        #    "user": jdata["user"],
+#        #    "password": "",
+#        #    "database": jdata["database"]
+#        #}
+#        #await ctx.channel.send(self.is_valid_date(year, month, day))
+#        if self.is_valid_date(year, month, day):
+#            try:
+#                insert_query = f"INSERT INTO my_note2 (name, year ,month, day,hours,mins) VALUES (%s, %s, %s, %s,%s ,%s)"
+#                values = (name,int(year) ,int(month), int(day),int(hours,int(mins)))
+#                connection = pymysql.connect(**self.db_config)
+#                print("Successfully connected to the database.")
+#
+#                cursor = connection.cursor()
+#                cursor.execute(insert_query, values)
+#
+#                connection.commit()
+#                print(ctx.channel)
+#                await ctx.channel.send("Data inserted successfully.")
+#                print("123")
+#            except pymysql.MySQLError as e:
+#        # 处理错误
+#                print("Error:", e)
+#                connection.rollback()
+#        else:
+#            await ctx.channel.send("無效的日期")
     
 
     @commands.command()
     async def delete_note(self, ctx, id):
-        db_config = {
-            "host": "localhost",
-            "user": jdata["user"],
-            "password": "",
-            "database": jdata["database"]
-        }
+        #db_config = {
+        #    "host": jdata["host"],
+        #    "user": jdata["user"],
+        #    "password": "",
+        #    "database": jdata["database"]
+        #}
         try:
             delete_query = "DELETE FROM my_note2 WHERE id = %s"
             values = (id,)
 
-            connection = pymysql.connect(**db_config)
+            connection = pymysql.connect(**self.db_config)
             print("Successfully connected to the database.")
 
             cursor = connection.cursor()
@@ -175,67 +231,4 @@ class MyCog(Cog_extension):
 
 async def setup(bot):
     await bot.add_cog(MyCog(bot))
-
-#import discord
-#from discord.ext import commands, tasks
-#from core.classes import Cog_extension
-#import pymysql
-#import asyncio
-#import datetime
-#import json
-#
-#with open('setting.json', 'r', encoding='utf-8') as jfile:
-#    jdata = json.load(jfile)
-#
-#class MyCog(Cog_extension):
-#    def __init__(self, *args, **kwargs):
-#        super().__init__(*args, **kwargs)
-#        self.channel = 0
-#        self.index = 0
-#        self.printer.start()
-#
-#    def cog_unload(self):
-#        self.printer.cancel()
-#
-#    @commands.command()
-#    async def set_channel(self,ctx,msg:int):
-#        self.channel = self.bot.get_channel(msg)
-#        await ctx.send(f"change to {self.channel.mention}")
-#
-#
-#    @tasks.loop(seconds=5.0)
-#    async def printer(self):
-#        print(self.index)
-#        if(self.index==0):
-#            await self.send_message()
-#        await self.channel.send(self.index)
-#        #await self.channel.send(self.index)
-#        self.index += 1
-#
-#
-#    async def send_message(self):
-#        self.channel = self.bot.get_channel(int(jdata["WELCOME"]))
-#
-#    @printer.before_loop
-#    async def before_printer(self):
-#        print('waiting...')
-#        await self.bot.wait_until_ready()
-#
-#async def setup(bot):
-#    await bot.add_cog(MyCog(bot))
-
-
-
-#class Task(Cog_extension):
-#    def __init__(self,*args,**kwargs):
-#        super().__init__(*args,**kwargs)
-#
-#        async def interval(self):
-#            await self.bot.wait_until_ready()
-#            self.channel = self.bot.get_channel(1137993729253318708)
-#            while not self.bot.is_closed():
-#                await self.channel.send("I'm running")
-#                await asyncio.sleep(5)
-#        self.bg_task = self.bot.loop.create_task(interval())
-#
 
